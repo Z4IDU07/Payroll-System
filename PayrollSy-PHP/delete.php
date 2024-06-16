59a350ccafdec9f77f1d1bbf56c54d44f227d054
zaidu <95475723+Z4IDU07@users.noreply.github.com>
2024-06-16 15:17:27 +0530

    Delete PayrollSy-PHP directory

--- PayrollSy-PHP/delete.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	require('db.php');
-	
-	$id=$_GET['emp_id'];
-	$query = "DELETE FROM employee WHERE emp_id=$id"; 
-	$result = mysqli_query($connection, ($query) or die ( mysql_error()));
-	header("Location: home_employee.php"); 
- ?>
